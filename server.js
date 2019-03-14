d616161d6a7a7c55a0aa923b0ffa1d2f85ee6180
cherianajay <cherianajay@yahoo.com>
2019-03-14 10:18:28 -0500

    Update server.js
    just testing changes to server

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ var http = require('http');
 http.createServer(function (req, res) {
     
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('Hello, world!');
+    res.end('Hello, world! this is a node application ');
     
 }).listen(process.env.PORT || 8080);
