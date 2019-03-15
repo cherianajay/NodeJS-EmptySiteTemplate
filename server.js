4111ba1320a55aaa3c42b87186107b87dc5ddddd
cherianajay <cherianajay@yahoo.com>
2019-03-14 20:48:16 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 var http = require('http');
 
 http.createServer(function (req, res) {
+    var res1="";
+    unirest.post('http://mockbin.com/request')
+.headers({'Accept': 'application/json', 'Content-Type': 'application/json'})
+.send({ "parameter": 23, "foo": "bar" })
+.end(function (response) {
+  console.log(response.body);
+        res1=response.body;
+});
     
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('Hello, world! this is a node application ');
+    res.end('Hello, world! this is a node application ' +res1);
     
 }).listen(process.env.PORT || 8080);
