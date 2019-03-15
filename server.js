767250cfc165f103e20c372e3ba8df9494a9db6b
cherianajay <cherianajay@yahoo.com>
2019-03-14 21:46:56 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,25 @@
-var http = require('http');
 var unirest = require('unirest');
+var express = require('express');
+var app = express(); 
+//http://localhost:3000/_getproduct/8821264 
 
-http.createServer(function (req, res) {
-    var res1="";
-    unirest.post('http://mockbin.com/request')
-.headers({'Accept': 'application/json', 'Content-Type': 'application/json'})
-.send({ "parameter": 23, "foo": "bar" })
-.end(function (response) {
-  console.log(response.body);
-        res1=response.body;
-});
-    
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('Hello, world! this is a node application ' +res1);
-    
-}).listen(process.env.PORT || 8080);
+app.get('/', function(req, res) { 
+       
+     
+       unirest.post('http://mockbin.com/request')
+       .headers({'Accept': 'application/json', 'Content-Type': 'application/json'})
+       .send({ "parameter": 23, "foo": "bar" })
+       .end(function (response) {
+           console.log(response.body.headersSize)
+
+           res.write(JSON.stringify(response.body));
+            res.end();
+       });
+
+
+       
+     });
+     
+     app.listen(process.env.PORT||3000, () => console.log('Example app listening on port !'))
+
+     
